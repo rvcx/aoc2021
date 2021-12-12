1a8c4d434b84e5505c81dc70fe10085f286ad913
Rob Shearer <rob@v.cx>
2021-12-11 22:39:01 -0700

    day 12

---
+++ 12/1.swift
@@ -0,0 +1,58 @@
+import aoclib
+
+var data = lines(file: "input.txt").map { $0.split(separator: "-").map { String($0) } }
+
+var nexts = [String: Set<String>]()
+var rooms = Set<String>()
+for l in data {
+  rooms.insert(l[0])
+  rooms.insert(l[1])
+  if var s = nexts[l[0]] {
+    s.insert(l[1])
+    nexts[l[0]] = s
+  } else {
+    var s = Set<String>()
+    s.insert(l[1])
+    nexts[l[0]] = s
+  }
+  if var s = nexts[l[1]] {
+    s.insert(l[0])
+    nexts[l[1]] = s
+  } else {
+    var s = Set<String>()
+    s.insert(l[0])
+    nexts[l[1]] = s
+  }
+}
+
+func explore(_ path: [String], visited: Set<String>) -> [[String]] {
+  print(path)
+  assert(!path.isEmpty)
+  var out = [[String]]()
+  for n in nexts[path[path.count - 1]] ?? Set<String>() {
+    print(n)
+    if !visited.contains(n) {
+      var np = path
+      np.append(n)
+      if n == "end" {
+        out.append(np)
+      } else {
+        var newv = visited
+        if n.allSatisfy({ $0.isLowercase }) {
+          newv.insert(n)
+        }
+        out += explore(np, visited: newv)
+      }
+    }
+  }
+  return out
+}
+
+
+var v = Set<String>()
+v.insert("start")
+var output = Set<[String]>(explore(["start"], visited: v))
+
+//print(nexts)
+print(output)
+print(output.count)
